86fa1610b09cd344ba35591d2d566b21a0c95e1f
fitriyadi <taufikfitriyadi27@gmail.com>
2022-07-27 08:09:33 +0700

    perbaiki footer

--- user/_footer.php
+++ user/_footer.php
@@ -4,7 +4,7 @@
             <div class="border-top pt-5">
               <p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Rumah Belajar Sragen | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>.Downloaded from <a href="https://themeslab.org/" target="_blank">Themeslab</a>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Rumah Belajar Sragen
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
             </div>
